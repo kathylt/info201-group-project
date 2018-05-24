151c8f0305df45b2688e44bdaac2e477c8c5df8f
kathylt <studentuser@Students-MacBook-Pro.local>
2018-05-23 17:26:33 -0700

    more

--- Map Plot.R
+++ Map Plot.R
@@ -4,30 +4,26 @@ library(plotly)
 library(RColorBrewer)
 library(stringr)
 
+library(leaflet)
 
 Sys.setlocale(category = "LC_ALL", locale = "C")
 df <- read.csv("police_killings.csv", stringsAsFactors = F)
 
-build_map_static <- function(df) {
-library(leaflet)
-
-df$raceethnicity <- factor(df$raceethnicity, levels = c("Black", "White", "Hispanic/Latino",
-                                                        "Asian/Pacific Islander"))
-df$gender <- factor(killings_data1$gender,
-                    levels = c("Male", "Female"))
-
-df$cause <- factor(killings_data1$cause,
-                   levels = c("Gunshot", "Taser", "Death in custody", "Struck by vehicle"))
 
 
-factpal <- colorFactor(topo.colors(4), df[, map_var])
+
+build_map <- function(df) {
+  
+  labs <- lapply(seq(nrow(df)), function(i) {
+    paste0( '<p>', df[i, "name"], '<p></p>', 
+            df[i, "city"], ', ', 
+            df[i, "state"],'</p>') 
+  })
+factpal <- colorFactor(topo.colors(10), df$cause)
   map <- leaflet(df) %>% 
     addTiles() %>% 
-  addCircleMarkers(radius = 1, label = paste0(df$name,"
-",df$city, ", ", df$state,"
-", df$month," ", df$day), color = ~factpal(map_var), opacity = .7) %>% 
-    addLegend(position = "bottomleft", pal = factpal, values = ~map_var,
-              title = str_to_title(map_var), na.label = "Not Collected")
+  addCircleMarkers(radius = 1, label = lapply(labs, HTML), color = ~factpal(cause), opacity = .7) %>% 
+    addLegend(position = "bottomleft", pal = factpal, values = ~cause, na.label = "Not Collected")
   map
 }
 
--- server.R
+++ server.R
@@ -1,14 +1,22 @@
 library(dplyr)
-
-
+library(leaflet)
 source("Map Plot.R")
+
 df <- read.csv("police_killings.csv", stringsAsFactors = F)
+
+df$raceethnicity <- factor(df$raceethnicity, levels = c("Black", "White", "Hispanic/Latino",
+                                                        "Asian/Pacific Islander"))
+df$gender <- factor(df$gender,
+                    levels = c("Male", "Female"))
+
+df$cause <- factor(df$cause,
+                   levels = c("Gunshot", "Taser", "Death in custody", "Struck by vehicle"))
+
 
 shinyServer(function(input, output) { 
   
   # Render a plotly object that returns your map
   output$Map <- renderLeaflet({ 
-    return(build_map(df, input$map_var))
-  }) 
-  
+    return(build_map(df))
+  })
 })
