33eae9026ed851f9f231964cbd87c8ac70a06957
zhouyifan0904 <zhouyf98@uw,edu>
2018-05-27 13:41:06 -0700

    updated scatterplot

--- max's_scatterplot.R
+++ max's_scatterplot.R
@@ -4,9 +4,21 @@ library(ggExtra)
 
 police_killings <- read.csv("police_killings.csv")
 
-p <- ggplot(police_killings, aes(as.numeric(county_income), as.numeric(share_white), color = armed)) +
-  geom_point() +
-  xlim(0, max(as.numeric(police_killings$county_income))) +
-  ylim(0, max(as.numeric(police_killings$share_white)))
 
-p <- ggMarginal(p, type = "histogram")
+max_plot_1 <- function(df) {
+	p <- ggplot(police_killings, aes(as.numeric(county_income), as.numeric(share_white), color = armed)) +
+  		geom_point() +
+  		xlim(0, max(as.numeric(police_killings$county_income))) +
+  		ylim(0, max(as.numeric(police_killings$share_white))) +
+  		xlab() +
+  		ylab()
+
+  	p <- ggMarginal(p, type = "histogram")
+
+  	return(p)
+}
+
+
+max_plot_2 <- function(df) {
+
+}
