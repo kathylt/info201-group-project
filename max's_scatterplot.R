b058631c8133ba5b95def6d1c2c402312d456ae2
kathylt <studentuser@Students-MacBook-Pro.local>
2018-05-27 21:37:11 -0700

    add color to scatter plot

--- max's_scatterplot.R
+++ max's_scatterplot.R
@@ -6,7 +6,7 @@ police_killings <- read.csv("police_killings.csv")
 
 max_plot_1 <- function(df, input_x, input_y, input_color) {
   df <- na.omit(df)
-	p <- ggplot(df, aes(as.numeric(df[ ,input_x]), as.numeric(df[ ,input_y]), color = input_color)) +
+	p <- ggplot(df, aes(as.numeric(df[ ,input_x]), as.numeric(df[ ,input_y]), color = df[, input_color])) +
   		geom_point(na.rm = T) +
   		xlim(0, max(as.numeric(df[[input_x]]), na.rm = T)) +
   		ylim(0, max(as.numeric(df[[input_y]]), na.rm = T)) +
